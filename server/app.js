2eb6548b6afabeff15fdb7cce8749f474a577e62
piusgori <piusgori@gmail.com>
2024-08-06 12:05:41 +0300

    make changes

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -50,6 +51,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
